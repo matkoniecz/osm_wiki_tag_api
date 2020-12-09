1828da9f28a26d0daeb8c2ef4a173296553886da
Mateusz Konieczny <matkoniecz@gmail.com>
2020-12-09 22:04:20 +0100

    drop translations due to buggy data items

--- pywik.py
+++ pywik.py
@@ -58,10 +58,11 @@ for page in site.allpages(namespace = [0]):
 
 for infobox in ["Template:ValueDescription", "Template:KeyDescription"]:
     root_page = pywikibot.Page(pywikibot.Site(), infobox)
-    for page in root_page.getReferences(namespaces=[0], content=True): #namespaces=self.opt.namespaces
-        compare_data(page.title())
-        #print(data_item_extractor.page_data(page.title()))
-        #print(template_extractor.page_data(page.title()))
+    for page in root_page.getReferences(namespaces=[0], content=True):
+        if page.title().find("Tag:") == 0 or page.title().find("Key:") == 0: #No translated pages as data items are borked there
+            compare_data(page.title())
+            #print(data_item_extractor.page_data(page.title()))
+            #print(template_extractor.page_data(page.title()))
 
 
 """
