3ae031622b5cde8c59557eefa9c91209fb251ab5
Mateusz Konieczny <matkoniecz@gmail.com>
2021-01-11 10:06:33 +0100

    add design sketch of taginfo access

---
+++ taginfo.py
@@ -0,0 +1,11 @@
+def count_appearances_of_tag(key, value):
+    pass
+
+def count_appearances_of_key(key):
+    pass
+
+def count_appearances_of_tag_historic_data(key, value, date): # offset in days??
+    pass
+
+def count_appearances_of_key_historic_data(key, date): # offset in days??
+    pass
