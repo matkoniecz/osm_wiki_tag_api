a4b7b01bb309ff4ca8461c369b6405830ddcd900
Mateusz Konieczny <matkoniecz@gmail.com>
2021-01-13 22:14:09 +0100

    list popular

--- attempt_taginfo.py
+++ attempt_taginfo.py
@@ -1,4 +1,49 @@
 import taginfo
+import links
+import time
 
+def show_new_popular(key, multiplier=1):
+    values = taginfo.get_all_values_of_key(key)
+    for entry in values:
+        value = entry["value"]
+        description = entry["description"]
+        if description == "" or description == None:
+            offset=100
+            delta = taginfo.count_new_appearances_of_tag_historic_data(key, value, offset)
+            if delta == None:
+                return # dropped into low use
+            if delta > 1000 * multiplier: # growing
+                print(key + "=" + value, "(increase by " + str(delta) + " in last " + str(offset) + " days):")
+                print(links.osm_wiki_page_link_from_tag(key, value))
+                print()
+
+show_new_popular("attraction")
+show_new_popular("water")
+show_new_popular("tunnel")
+show_new_popular("public_transport")
+show_new_popular("healthcare")
+show_new_popular("building", 8)
+show_new_popular("amenity")
+show_new_popular("man_made", 4)
+show_new_popular("highway")
+show_new_popular("natural", 0.5)
+show_new_popular("landuse")
+show_new_popular("shop", 0)
+show_new_popular("historic", 4)
+show_new_popular("power")
+show_new_popular("leisure")
+show_new_popular("waterway")
+show_new_popular("barrier", 0.5)
+show_new_popular("place", 0.1)
+show_new_popular("tourism", 0.5)
+show_new_popular("wall")
+show_new_popular("footway")
+show_new_popular("bridge")
+show_new_popular("service")
+show_new_popular("railway")
+show_new_popular("access", 0.1)
+show_new_popular("bridge")
+time.sleep(10101010101010101010110100101)
 print(taginfo.count_appearances_of_key("building"))
 print(taginfo.count_appearances_of_tag("landuse", "yes"))
+print(taginfo.get_all_data_about_key_use("shop"))
--- taginfo.py
+++ taginfo.py
@@ -1,17 +1,16 @@
 import urllib
 import urllib.request
+import urllib.parse
 import json
 
 def count_appearances_of_tag(key, value):
-    url = "https://taginfo.openstreetmap.org/api/4/tag/stats?key=" + key + "&value=" + value
+    url = "https://taginfo.openstreetmap.org/api/4/tag/stats?key=" + urllib.parse.quote(key) + "&value=" + urllib.parse.quote(value)
     data = json_response_from_url(url)
-    print(data)
     return data['data'][0]['count']
 
 def count_appearances_of_key(key):
-    url = "https://taginfo.openstreetmap.org/api/4/key/stats?key=" + key
+    url = "https://taginfo.openstreetmap.org/api/4/key/stats?key=" + urllib.parse.quote(key)
     data = json_response_from_url(url)
-    print(data)
     return data['data'][0]['count']
 
 def count_appearances_from_wiki_page_title(osm_wiki_page_title):
@@ -26,13 +25,54 @@ def count_appearances_from_wiki_page_title(osm_wiki_page_title):
     else:
         raise "unhandled"
 
-def count_appearances_of_tag_historic_data(key, value, date): # offset in days??
-    pass
+def get_all_data_about_key_use(key):
+    url = "https://taginfo.openstreetmap.org/api/4/key/chronology?key=" + urllib.parse.quote(key)
+    data = json_response_from_url(url)
+    return data['data']
 
-def count_appearances_of_key_historic_data(key, date): # offset in days??
-    pass
+def get_all_data_about_tag_use(key, value):
+    url = "https://taginfo.openstreetmap.org/api/4/tag/chronology?key=" + urllib.parse.quote(key) + "&value=" + urllib.parse.quote(value)
+    data = json_response_from_url(url)
+    return data['data']
+
+def count_new_appearances_of_tag_historic_data_from_deltas(data, days_ago):
+    if data == []:
+        return None
+    diff = 0
+    for offset in range(1, days_ago):
+        if len(data) >= offset:
+            diff += data[-offset]["nodes"]
+            diff += data[-offset]["ways"]
+            diff += data[-offset]["relations"]
+    return diff
+
+def count_new_appearances_of_tag_historic_data(key, value, days_ago):
+    data = get_all_data_about_tag_use(key, value)
+    return count_new_appearances_of_tag_historic_data_from_deltas(data, days_ago)
+
+def count_new_appearances_of_key_historic_data(key, days_ago):
+    data = get_all_data_about_key_use(key)
+    return count_new_appearances_of_tag_historic_data_from_deltas(data, days_ago)
+
+def get_all_values_of_key(key):
+    page = 1
+    while True:
+        data = get_page_of_key_values(key, page)
+        for entry in data:
+            yield entry
+        page += 1
+        if len(data) < entries_per_page():
+            break
+
+def entries_per_page():
+    return 999
+
+def get_page_of_key_values(key, page):
+    url = "https://taginfo.openstreetmap.org/api/4/key/values?key=" + urllib.parse.quote(key) + "&page=" + str(page) + "&rp=" + str(entries_per_page()) + "&sortname=count_all&sortorder=desc"
+    return json_response_from_url(url)["data"]
 
 def json_response_from_url(url):
+    url = url.replace(" ", "%20")
     try:
         data = urllib.request.urlopen(url).read()
         return json.loads(data)
