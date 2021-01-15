e6220ab9c7666f88f16cc541857d94a5dc51697e
Mateusz Konieczny <matkoniecz@gmail.com>
2021-01-15 13:12:10 +0100

    extends links file

--- links.py
+++ links.py
@@ -2,3 +2,13 @@ def osm_wiki_page_link(page_name):
     url = "https://wiki.openstreetmap.org/wiki/" + page_name
     url = url.replace(" ", "_")
     return url
+
+def osm_wiki_page_link_from_tag(key, value):
+    url = "https://wiki.openstreetmap.org/wiki/Tag:" + key + "=" + value
+    url = url.replace(" ", "_")
+    return url
+
+def osm_wiki_page_link_from_key(key):
+    url = "https://wiki.openstreetmap.org/wiki/Key:" + key
+    url = url.replace(" ", "_")
+    return url
