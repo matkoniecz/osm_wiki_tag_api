72ae0e35c489228be72bd4c6d0ee163a3de59012
Mateusz Konieczny <matkoniecz@gmail.com>
2022-08-28 07:42:44 +0200

    list unusual statuses

---
+++ status_lister.py
@@ -0,0 +1,10 @@
+import csv
+import re
+
+with open('infobox_data.csv') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        status = row[3]
+        status = re.sub('<!--.*-->', '', status)
+        if status.strip().lower() not in ["", "approved", "in use", "de facto", "imported", "import", "discardable", "deprecated", "proposed", "defacto", "inuse", "rejected", "abandoned", "draft", "obsolete"]:
+            print("*", row[0], row[0]+"?action=edit", status)
