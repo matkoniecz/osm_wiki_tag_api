f4edd544b1eb4f2b621b5544c397e84de2725242
Mateusz Konieczny <matkoniecz@gmail.com>
2022-09-02 08:29:01 +0200

    drop status=import from valid ones

--- status_lister.py
+++ status_lister.py
@@ -6,5 +6,5 @@ with open('infobox_data.csv') as csvfile:
     for row in spamreader:
         status = row[3]
         status = re.sub('<!--.*-->', '', status)
-        if status.strip().lower() not in ["", "approved", "in use", "de facto", "imported", "import", "discardable", "deprecated", "proposed", "rejected", "abandoned", "draft", "obsolete"]:
+        if status.strip().lower() not in ["", "approved", "in use", "de facto", "imported", "discardable", "deprecated", "proposed", "rejected", "abandoned", "draft", "obsolete"]:
             print("*", row[0], row[0]+"?action=edit", status)
